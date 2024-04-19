eced6d7ca16ccb1b22afbd4d7064374f6ebe0f3d
Esraa102 <manalellban9@gmailcom>
2024-04-19 21:25:14 +0200

    create user model

--- backend/middleware/errorHanlder.js
+++ backend/middleware/errorHanlder.js
@@ -1,5 +1,4 @@
 export const errorHanlder = (err, req, res, next) => {
-  console.log(res);
   const status = res.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(status).json({ message });
