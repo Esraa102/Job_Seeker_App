538006169b2be35a58a62935cb21b76a25328200
Esraa102 <manalellban9@gmailcom>
2024-04-19 21:18:39 +0200

    create error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectToDB } from "./config/connectDB.js";
 dotenv.config();
+import cors from "cors";
+import { errorHanlder } from "./middleware/errorHanlder.js";
+import { userRouter } from "./routes/user.route.js";
 const app = express();
 connectToDB();
 const port = process.env.PORT || 5001;
 
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use("/api/user", userRouter);
+app.use(errorHanlder);
 app.listen(port, () => {
   console.log("Server Is Running On", port);
 });
