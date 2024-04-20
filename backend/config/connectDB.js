dc88ce213edf9678aa257e7452e53bee1349cce3
Esraa102 <manalellban9@gmailcom>
2024-04-20 05:34:14 +0200

    update error hanlder middleware

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 export const connectToDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION_URI);
-    console.log("Connected");
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+  mongoose
+    .connect(process.env.MONGODB_CONNECTION_URI)
+    .then(() => {
+      console.log("Connected");
+    })
+    .catch((error) => {
+      console.log(error);
+      process.exit(1);
+    });
 };
--- backend/middleware/errorHanlder.js
+++ backend/middleware/errorHanlder.js
@@ -1,5 +1,9 @@
 export const errorHanlder = (err, req, res, next) => {
-  const status = res.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  let status = res.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+  if (err.name === "CaseError") {
+    message = `Resource not found, invalid ${err.path}`;
+    status = 400;
+  }
   return res.status(status).json({ message });
 };
