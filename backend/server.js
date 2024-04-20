57b4e620a3361b17351a71a39ddfba8240a70042
Esraa102 <manalellban9@gmailcom>
2024-04-20 05:09:54 +0200

    set cloudinary

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,40 @@ import dotenv from "dotenv";
 import { connectToDB } from "./config/connectDB.js";
 dotenv.config();
 import cors from "cors";
+import cloudinary from "cloudinary";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 import { errorHanlder } from "./middleware/errorHanlder.js";
 import { userRouter } from "./routes/user.route.js";
+
 const app = express();
 connectToDB();
+
 const port = process.env.PORT || 5001;
 
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUNDINARY_API_SECRET,
+});
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 app.use("/api/user", userRouter);
 app.use(errorHanlder);
 app.listen(port, () => {
