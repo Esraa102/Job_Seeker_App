6ed3810442a34691eacec3decf94339a92c19034
Esraa102 <manalellban9@gmailcom>
2024-04-20 05:46:16 +0200

    update user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,23 +1,33 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
-const userSchema = new mongoose.Schema({
-  role: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    role: {
+      type: String,
+      required: [true, "Please Provide Your role"],
+      enum: ["Job Seeker", "Employer"],
+    },
+    username: {
+      type: String,
+      required: [true, "Please provide your username"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please Provide your email"],
+      unique: [true, "User of this email is already exist"],
+      validate: [validator.isEmail, "Please provide valid email"],
+    },
+    phone: {
+      type: Number,
+      required: [true, "Please provide your phone number"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+    },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model("users", userSchema);
