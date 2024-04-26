b43aec5ac9db56075cf14da71db784daf71ceac5
Esraa102 <manalellban9@gmailcom>
2024-04-26 20:00:36 +0300

    upate origin url in the backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://job-seeker-app-1.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
