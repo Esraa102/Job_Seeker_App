fdcc55fa169327388415163f3b7d0b208720b17a
Esraa102 <manalellban9@gmailcom>
2024-04-26 19:46:40 +0300

    ready for deploying

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,12 @@ import { errorHanlder } from "./middleware/errorHanlder.js";
 import { userRouter } from "./routes/user.route.js";
 import { applicationRouter } from "./routes/application.route.js";
 import { jobRouter } from "./routes/job.router.js";
-
+import path from "path";
 const app = express();
 connectToDB();
 
 const port = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -29,8 +29,14 @@ app.use(
 app.use("/api/user", userRouter);
 app.use("/api/application", applicationRouter);
 app.use("/api/jobs", jobRouter);
-
 app.use(errorHanlder);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 app.listen(port, () => {
   console.log("Server Is Running On", port);
 });
